ad9b200539764e6a75cec663d2ffe8b0b98991a9
CuteRyca <rzhan114@ucsc.edu>
2021-06-26 02:38:13 -0700

    add starfield

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,10 +6,15 @@ class Play extends Phaser.Scene {
 
     }
     preload(){
-
+        this.load.image('rocket','./assets/rocket.png');
+        this.load.image('spaceship','./assets/spaceship.png');
+        this.load.image('starfield','./assets/starfield.png');
     }
     create(){
         this.add.text(20, 20, "Rocket Partrol Play");
+        
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
+        
         // green UI background
         this.add.rectangle(0,borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
         
@@ -18,8 +23,9 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height , 0xFFFFFF).setOrigin(0,0);
 
+       
     }
     update(){
-
+        this.starfield.tilePositionX -= 4;
     }
 }
